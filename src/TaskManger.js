93d2a63c3d9045997152181457f0ab590f6cd48e
Bennett Fife <33855430+bennettsf@users.noreply.github.com>
2024-03-13 19:43:44 -0700

    cleaned up with comments

--- src/TaskManger.js
+++ src/TaskManger.js
@@ -1,23 +1,27 @@
 import React, { useState } from "react";
 
 function TaskManger() {
+  // create useStates of our tasks array and task count
   const [tasks, setTasks] = useState([]);
   const [taskId, setTaskId] = useState(1);
 
   const addTask = () => {
+    // create a new task object to add
     const newTask = {
       id: taskId,
       title: "Task " + taskId,
       completed: false,
     };
-    
+    // append it to the end of the current tasks using spread syntax
     setTasks([...tasks, newTask]);
+    // update the task count used for task.id and task.title objects
     setTaskId(taskId + 1);
   };
 
   const toggleTask = (id) => {
-    // iterate tasks
+    // iterate tasks and create new array
     const updatedTasks = tasks.map(task => {
+        // if the task ID exists, change the 'completed' variable
         if (task.id === id){
             return {
                 ...task,
@@ -26,6 +30,7 @@ function TaskManger() {
         }
         return task
     })
+    // update the tasks array with the mapped out version
     setTasks(updatedTasks)
   };
 
